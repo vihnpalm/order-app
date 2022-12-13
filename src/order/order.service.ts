dfd3abacc3ab20b55a08aacee79758825583816b
vihnpalm <vplm.vn@gmail.com>
2022-12-13 15:21:11 +0700

    git commit

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,10 +1,9 @@
-import { Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { Order } from './interfaces/order.interface';
 import { CreateOrderDTO } from './dtos/create-order.dto';
 import { ClientProxy } from '@nestjs/microservices';
-import { Inject } from '@nestjs/common/decorators';
     
 @Injectable()
 export class OrderService {
