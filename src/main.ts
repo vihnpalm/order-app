2791d96e9f1c54ec4a453f2048b7e55a80b90bc5
vihnpalm <vplm.vn@gmail.com>
2022-12-13 14:40:29 +0700

    git commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { OrderModule } from './order/order.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(OrderModule);
   app.connectMicroservice({
     transport: Transport.TCP,
   })
