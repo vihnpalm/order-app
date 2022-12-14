533bc46bd85ddc004e3b6a5129c89aca6e01edce
vihnpalm <vplm.vn@gmail.com>
2022-12-14 09:10:30 +0700

    git commit

--- src/order/shared/pipes/validate-object-id.pipes.ts
+++ src/order/shared/pipes/validate-object-id.pipes.ts
@@ -5,9 +5,9 @@ import * as mongoose from 'mongoose';
 export class ValidateObjectId implements PipeTransform<string> {
   async transform(value: string, metadata: ArgumentMetadata) {
     const isValid = mongoose.Types.ObjectId.isValid(value);
-    // if (!isValid) {
-    //     throw new BadRequestException('Invalid ID!');
-    // }
+     if (!isValid) {
+         throw new BadRequestException('Invalid ID!');
+     }
     return value;
   }
 }
